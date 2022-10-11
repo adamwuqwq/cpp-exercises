1e4e4a1ca78fff1921aca8b4d537c7c324b561cb
Yifan Wu <adamwuqwq@gmail.com>
2022-10-11 11:33:36 +0900

    Fix problem in ex01_1

--- ex01/ex01_1.cpp
+++ ex01/ex01_1.cpp
@@ -2,7 +2,7 @@
 
 bool parity(unsigned long x)
 {
-    bool parity = 0;
+    bool parity = 1;
     while (x)
     {
         parity = !parity;
@@ -13,14 +13,17 @@ bool parity(unsigned long x)
 
 int main(void)
 {
-    // Test case #0: should be even(1)
+    // Test case #0: should be odd(0)
     std::cout << "1:" << parity(1) << std::endl;
 
-    // Test case #1: should be odd(0)
+    // Test case #1: should be even(1)
     std::cout << "114:" << parity(114) << std::endl;
 
-    // Test case #2: should be even(1)
+    // Test case #2: should be odd(0)
     std::cout << "817:" << parity(817) << std::endl;
+
+    // Test case #3: should be odd(1)
+    std::cout << "17298:" << parity(17298) << std::endl;
 
     return 0;
 }
