4a1fc43bacd81e16dea62f8582015ab23c5c6d48
Yifan Wu <adamwuqwq@gmail.com>
2022-10-17 11:04:37 +0900

    Implemented addition using bitwise operation

--- ex01/ex01_2.cpp
+++ ex01/ex01_2.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+// Addition only using bitwise operation
+unsigned long long add(unsigned long a, unsigned long b)
+{
+    while (b != 0)
+    {
+        unsigned long carry = a & b;
+        a = a ^ b;
+        b = carry << 1;
+    }
+    return a;
+}
+
+// Multiplication only using bitwise operation
 unsigned long long multiply(unsigned long a, unsigned long b)
 {
     unsigned long long result = 0;
@@ -7,7 +20,7 @@ unsigned long long multiply(unsigned long a, unsigned long b)
     {
         if (b & 1)
         {
-            result += a;
+            result = add(result, a);
         }
         a = a << 1;
         b = b >> 1;
@@ -21,7 +34,7 @@ int main(void)
     std::cout << "2*3:" << multiply(2, 3) << std::endl;
 
     // Test case #1: should be 92340
-    std::cout << "114*810:" << multiply(114, 810)  << std::endl;
+    std::cout << "114*810:" << multiply(114, 810) << std::endl;
 
     // Test case #2: should be 4109358
     std::cout << "1914*2147:" << multiply(1914, 2147) << std::endl;
