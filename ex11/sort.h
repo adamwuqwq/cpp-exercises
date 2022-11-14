09cfb3a10fb09d1ca9bbb292da3efe991d63bd17
Yifan Wu <adamwuqwq@gmail.com>
2022-11-14 10:37:52 +0900

    Updated VSCode config, Finished ex_11

---
+++ ex11/sort.h
@@ -0,0 +1,23 @@
+#ifndef SORT_H
+#define SORT_H
+#include <iostream>
+
+namespace PL4
+{
+    // Insertion sort
+    template <class BiDirIt>
+    void sort(BiDirIt begin, BiDirIt end)
+    {
+        for (BiDirIt i = begin; i != end; i++)
+        {
+            BiDirIt j = i;
+            while (j != begin && *std::prev(j) > *j)
+            {
+                std::swap(*j, *std::prev(j));
+                j--;
+            }
+        }
+    }
+}
+
+#endif
---
+++ ex11/sort_test.cpp
@@ -0,0 +1,27 @@
+#include "sort.h"
+#include <vector>
+
+// Show all the values in vector<int>
+void show_values(std::vector<int> arr)
+{
+    for (auto i : arr)
+    {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main(void)
+{
+    // Declare an vector<int> for test
+    std::vector<int> arr = {114, 810, 798, 1999, 3, -1, 6592};
+
+    // Before sort
+    show_values(arr);
+
+    // After sort
+    PL4::sort(arr.begin(), arr.end());
+    show_values(arr);
+    
+    return 0;
+}
